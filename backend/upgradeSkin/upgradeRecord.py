e8c8d21f64af93249ede5f8ae8dc3970c733a57f
kimduyminh <minhkimfp212@gmail.com>
2025-04-30 09:49:39 +0700

    feat: add all upgrade model and logic, still some flaws that need to be fixed in other parts

---
+++ backend/upgradeSkin/upgradeRecord.py
@@ -0,0 +1,9 @@
+class upgradeRecord:
+    def __init__(self):
+        self.userID=""
+        self.streak = 0
+        self.userWeaponID = 0
+        self.expectedWeaponID = 0
+        self.successRate = 0
+        self.upgradeRate = 0
+        
